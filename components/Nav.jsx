53c5b5a0dc9d9f2b689c123896d03336f377e270
MD Najrul Islam Sumon <nazrul2517@gmail.com>
2023-08-12 22:30:19 +0600

    change p

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -18,7 +18,7 @@ const Nav = () => {
           height={30}
           className='object-contain'
         />
-        <p className='logo_text'>Promptopia</p>
+        <p className='logo_text'>Thanks Adit</p>
       </Link>
 
         {/* Desktop Navigation */}
