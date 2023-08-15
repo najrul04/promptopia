a4098621f2df7e3906f6cd9c2f0cab04ad3b095b
MD Najrul Islam Sumon <nazrul2517@gmail.com>
2023-08-15 17:00:23 +0600

    implementing mongoose

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import { NextAuth } from "next-auth";
+import GoogleProvider from 'next-auth/providers/google';
 
-const route = () => {
-  return (
-    <div>route</div>
-  )
-}
 
-export default route
+const handler = NextAuth({
+    providers: [
+        GoogleProvider({
+            clientId: process.env.Google_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        })
+    ],
+    async session({session}) {
+
+    },
+    async signIn({profile}){
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+})
+
+export {handler as GET, handler as POST};
