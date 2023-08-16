b2202f56615f7e3a35faf1eaf9262ff5a021a791
MD Najrul Islam Sumon <nazrul2517@gmail.com>
2023-08-16 17:45:54 +0600

    adding real data using session

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -8,7 +8,7 @@ import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 import Provider from './Provider';
 
 const Nav = () => {
-    const isUserLoggedIn = true;
+    const { data:session } = useSession();
 
     const [providers, setProviders] = useState(null);
     const [toggleDropdown, setToggleDropdown] = useState(false)
