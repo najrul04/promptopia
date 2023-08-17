e89b8cfd418b6386c4114b3fd8008bd8b9fdfec8
MD Najrul Islam Sumon <nazrul2517@gmail.com>
2023-08-17 21:57:43 +0600

    remote use client from layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,33 +1,28 @@
-"use client";
+import "@styles/globals.css";
 
-import '@styles/globals.css';
-
-import Provider from 'components/Provider'
-import Nav from '@components/Nav';
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
 
 export const metadata = {
-    title: "promptopia",
-    description: "Discover & Share Ai Prompts"
-}
+  title: "Promptopia",
+  description: "Discover & Share AI Prompts",
+};
 
-const RootLayout = ({children}) => {
-  return (
-    <html lang='en'>
-        <body>
+const RootLayout = ({ children }) => (
+  <html lang='en'>
+    <body>
+      <Provider>
+        <div className='main'>
+          <div className='gradient' />
+        </div>
 
-        <Provider>
-            <div className="main">
-                <div className='gradient'/>
-            </div>
-            <main className='app'>
-                <Nav/>
-                {children}
-            </main>
-        </Provider>
-        
-        </body>
-    </html>
-  )
-}
+        <main className='app'>
+          <Nav />
+          {children}
+        </main>
+      </Provider>
+    </body>
+  </html>
+);
 
 export default RootLayout;
